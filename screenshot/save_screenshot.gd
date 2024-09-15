12e8d74767a354dc5350957b9383e1541c9da1fa
francosorbello <franco.sorbelo@gmail.com>
2024-09-14 21:01:28 -0300

    more screenshot stuff

--- screenshot/save_screenshot.gd
+++ screenshot/save_screenshot.gd
@@ -9,7 +9,8 @@ func _on_file_dialog_dir_selected(dir:String) -> void:
     img.save_png(dir + "/screenshot.png")
 
 func _on_button_pressed() -> void:
-    $FileDialog.popup()
+    # $FileDialog.popup()
+    download_on_web()
 
 func _on_file_dialog_file_selected(path:String) -> void:
     screenshot_tooltip_manager.toggle_tooltip_screenshot()
@@ -20,3 +21,13 @@ func _on_file_dialog_file_selected(path:String) -> void:
         print("Screenshot saved to: " + path)
     else:
         print("Error saving screenshot to: " + path)
+
+func download_on_web():
+    var img = await _get_image() 
+    var result_buffer = img.save_png_to_buffer()
+    JavaScriptBridge.download_buffer(result_buffer, "screenshot.png")
+
+func _get_image():
+    screenshot_tooltip_manager.toggle_tooltip_screenshot()
+    await get_tree().create_timer(0.5).timeout
+    return viewport.get_texture().get_image() 
